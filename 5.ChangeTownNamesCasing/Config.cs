2a84a2f3cd7480531a3e4e4a1a8506e4b56b3d6d
Anastasiya.Ivanova <agb66666@gmail.com>
2022-07-24 17:48:43 +0300

    Added solution 5.

---
+++ 5.ChangeTownNamesCasing/Config.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.ChangeTownNamesCasing
+{
+    public static class Config
+    {
+        public static string ConfigName = @"Server=DESKTOP-BGO0R35\SQLEXPRESS;Initial Catalog=MinionsDB;Integrated Security=true";
+    }
+}
